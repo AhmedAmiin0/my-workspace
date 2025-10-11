e16dc8a89912f015f2b1528d7764aef17b98522f
ahmed amin <ahmed.amin@alrajhitakaful.com>
2025-10-11 13:43:54 +0300

    fixes

--- backend-apps/customer-backend/start-dev.sh
+++ backend-apps/customer-backend/start-dev.sh
@@ -1,17 +1,19 @@
+#!/bin/sh
 
+cd /app/backend-apps
 
 npx nx build customer-backend --configuration=development --watch &
 WEBPACK_PID=$!
 
 echo "Waiting for initial build..."
-while [ ! -f /app/dist/backend/customer-backend/main.js ]; do
+while [ ! -f /app/backend-apps/dist/customer-backend/main.js ]; do
   sleep 1
 done
 echo "Initial build complete!"
 npx nodemon \
-  --watch /app/dist/backend/customer-backend \
+  --watch /app/backend-apps/dist/customer-backend \
   --delay 500ms \
   --verbose \
-  /app/dist/backend/customer-backend/main.js
+  /app/backend-apps/dist/customer-backend/main.js
 kill $WEBPACK_PID
 
